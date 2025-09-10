3340f0740881416328f47ca57e1e9779a35db704
Sander Vanryckeghem <78733641+SanderVanryckeghem@users.noreply.github.com>
2025-09-10 22:20:55 +0200

    create pages

---
+++ soccer-stats-frontend/src/app/components/match-form/match-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-match-form',
+  imports: [],
+  templateUrl: './match-form.html',
+  styleUrl: './match-form.scss'
+})
+export class MatchForm {
+
+}
---
+++ soccer-stats-frontend/src/app/components/player-form/player-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-form',
+  imports: [],
+  templateUrl: './player-form.html',
+  styleUrl: './player-form.scss'
+})
+export class PlayerForm {
+
+}
