96f192b0017c36627fdadf44910f5136b8bfe7ef
Sander Vanryckeghem <78733641+SanderVanryckeghem@users.noreply.github.com>
2025-09-12 15:33:54 +0200

    Update routing

--- soccer-stats-frontend/src/app/app.routes.ts
+++ soccer-stats-frontend/src/app/app.routes.ts
@@ -1,3 +1,4 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
@@ -8,15 +9,51 @@ export const routes: Routes = [
   },
   { 
     path: 'teams', 
-    loadComponent: () => import('./components/teams/teams').then(m => m.TeamsComponent)
+    loadComponent: () => import('./components/teams/teams.component').then(m => m.TeamsComponent)
+  },
+  { 
+    path: 'teams/new', 
+    loadComponent: () => import('./components/team-form/team-form').then(m => m.TeamFormComponent)
+  },
+  { 
+    path: 'teams/:id/edit', 
+    loadComponent: () => import('./components/team-form/team-form').then(m => m.TeamFormComponent)
+  },
+  { 
+    path: 'teams/:id', 
+    loadComponent: () => import('./components/team-detail/team-detail').then(m => m.TeamDetailComponent)
+  },
+  { 
+    path: 'teams/:teamId/players/new', 
+    loadComponent: () => import('./components/player-form/player-form').then(m => m.PlayerFormComponent)
   },
   { 
     path: 'players', 
     loadComponent: () => import('./components/players/players').then(m => m.PlayersComponent)
   },
   { 
+    path: 'players/:id/edit', 
+    loadComponent: () => import('./components/player-form/player-form').then(m => m.PlayerFormComponent)
+  },
+  { 
+    path: 'players/:id', 
+    loadComponent: () => import('./components/player-detail/player-detail').then(m => m.PlayerDetailComponent)
+  },
+  { 
     path: 'matches', 
     loadComponent: () => import('./components/matches/matches').then(m => m.MatchesComponent)
+  },
+  { 
+    path: 'matches/new', 
+    loadComponent: () => import('./components/match-form/match-form').then(m => m.MatchFormComponent)
+  },
+  { 
+    path: 'matches/:id/edit', 
+    loadComponent: () => import('./components/match-form/match-form').then(m => m.MatchFormComponent)
+  },
+  { 
+    path: 'matches/:id', 
+    loadComponent: () => import('./components/match-detail/match-detail').then(m => m.MatchDetailComponent)
   },
   { 
     path: 'stats', 
